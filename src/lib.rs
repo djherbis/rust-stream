4881c76de01f5653b862b966df3263dd39b9fec2
djherbis <djherbis@gmail.com>
2017-12-14 22:19:44 -0800

    stashing this attempt
    Signed-off-by: djherbis <djherbis@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,10 @@ extern crate alloc;
 
 #[macro_use]
 extern crate lazy_static;
+extern crate futures;
 
 pub mod buffer;
 pub mod stream;
 pub mod broadcast;
 pub mod next_reader;
+pub mod futurecond;
